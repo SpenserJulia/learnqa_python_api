303432ceb6ab5735bc8238ea2605a7ef873e8381
SpenserJulia <juliajul_spens@bk.ru>
2021-08-16 01:36:11 +0300

    Get requests to api

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# import requests
+import requests
 
-# response = requests.get("https://playground.learnqa.ru/api/hello")
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 
-# print(response.text)
+print(response.text)
 
-print("Hello from Julia Spenser")
