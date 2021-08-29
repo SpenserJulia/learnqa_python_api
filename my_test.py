7faac9e92823c5c8ea1012e3398934a212ec57fa
SpenserJulia <juliajul_spens@bk.ru>
2021-08-30 01:30:11 +0300

    Add assert for 15 simbols from consol

---
+++ my_test.py
@@ -0,0 +1,6 @@
+class TestShortPhrase:
+    def test_short_phrase(self):
+        phrase = input("Set a phrase:")
+        print(f"{phrase}")
+
+        assert len(phrgiase) <= 15, "Phrase longer then 15 simbols"
