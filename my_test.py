73670fe10cfd413196131abffe1f5df762e33e4c
SpenserJulia <juliajul_spens@bk.ru>
2021-09-03 01:10:10 +0300

    Forming FrameWork

--- my_test.py
+++
@@ -1,6 +0,0 @@
-class TestShortPhrase:
-    def test_short_phrase(self):
-        phrase = input("Set a phrase:")
-        print(f"{phrase}")
-
-        assert len(phrase) <= 15, "Phrase longer then 15 simbols"
