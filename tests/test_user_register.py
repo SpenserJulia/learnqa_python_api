2d1b10ca34971e97c65152a828b570fc58422718
SpenserJulia <juliajul_spens@bk.ru>
2021-09-03 03:33:14 +0300

    Add positive and negative test for user method

---
+++ tests/test_user_register.py
@@ -0,0 +1,76 @@
+import requests
+import pytest
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from datetime import datetime
+
+
+class TestUserRegister(BaseCase):
+    url_register = "https://playground.learnqa.ru/api/user/"
+    exclude_params = [
+        ('password'),
+        ('username'),
+        ('firstName'),
+        ('lastName'),
+        ('email')
+    ]
+
+    def setup(self):
+        self.base_part = "learnqa"
+        self.domain = "example.com"
+        self.random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{self.base_part}{self.random_part}@{self.domain}"
+
+        self.data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+        }
+
+    def test_create_user_successfully(self):
+        self.data['email'] = self.email
+
+        response = requests.post(self.url_register, data=self.data)
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
+    def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
+        self.data['email'] = email
+
+        response = requests.post(self.url_register, data=self.data)
+        Assertions.assert_code_status(response, 400)
+        Assertions.assert_answer_text(response, f"Users with email '{email}' already exists")
+
+    def test_incorrect_email_without_at(self):
+        email = f"{self.base_part}{self.random_part}{self.domain}"
+        self.data['email'] = email
+
+        response = requests.post(self.url_register, data=self.data)
+        Assertions.assert_code_status(response, 400)
+        Assertions.assert_answer_text(response, "Invalid email format")
+
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_without_some_params(self, condition):
+        self.data['email'] = self.email
+
+        del self.data[condition]
+
+        response = requests.post(self.url_register, data=self.data)
+        Assertions.assert_code_status(response, 400)
+        Assertions.assert_answer_text(response, f"The following required params are missed: {condition}")
+
+    def test_with_short_email(self):
+        self.data['email'] = 'q'
+
+        response = requests.post(self.url_register, data=self.data)
+        Assertions.assert_code_status(response, 400)
+        Assertions.assert_answer_text(response, "The value of 'email' field is too short")
+
+    def test_with_long_email(self):
+        self.data['email'] = 'testtestlooooooooooooooooooooooooooooooooooooooooooooooongloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongloooooooooooooooooooooooooooooooooooooooooooooongloooooooooooooooooooooooooooooooooooooooooooongloooooooooooooooooooong@email.com'
+
+        response = requests.post(self.url_register, data=self.data)
+        Assertions.assert_code_status(response, 400)
+        Assertions.assert_answer_text(response, "Invalid email format")
